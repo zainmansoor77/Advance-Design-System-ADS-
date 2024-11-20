adcdf4aadd3b3d618475176557affdaaf911e2e9
Zain Mansoor <zainmansoor77@gmail.com>
2024-11-20 09:53:11 +0100

    Getting started with ADS

---
+++ Getting Started/First_workspace/workspace.ads
@@ -0,0 +1,34 @@
+<ADSWorkspace Revision="4" Version="100">
+    <Workspace Name="">
+        <LibraryDefs Name="lib.defs" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_pelib" />
+        <Library Name="ads_serdes_ref_channel" />
+        <Library Name="ads_quantum" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_datacmps" />
+        <Library Name="Simulation_Sequencing" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <Library Name="First_workspace_lib" />
+        <Cell Name="First_workspace_lib:first schematic" />
+        <Cell Name="First_workspace_lib:First Layout" />
+        <Log Name="search_history.log" />
+    </Workspace>
+</ADSWorkspace>
